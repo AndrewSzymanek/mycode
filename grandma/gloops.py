0ec5af32e49a4a544fcb7d5909d0aaaf97b9d0e0
AndrewSzymanek <andrew.szymanek@comcast.net>
2020-09-23 19:23:15 +0000

    more forloops

---
+++ grandma/gloops.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+room = ["sausage biscuit", "plain", "gravy on top"]
+
+for biscuit in room:
+    print("flogged!")
+
+hallway = ["Woody", "Buzz", "Bo Peep", "Mr. Potato head"]
+
+for toy in hallway:
+    toy_removed = hallway.remove(toy)
+    print(toy_removed)
